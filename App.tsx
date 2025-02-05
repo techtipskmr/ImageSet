1bb89d71a161bcf259a5aee5fd00bbc3439999b6
techtipskmr <techtipskmr@gmail.com>
2025-02-05 12:01:51 +0530

    Added Nothing

--- App.tsx
+++ App.tsx
@@ -1,85 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [count,setCount] = useState(10)
+  const [text1, setText1] = useState("")
+   
+  const onpress1=()=>{
+    setCount(count+10)
+  }
+  const student:string = "Aijaz";
+  const onpress=(name1:string)=>{
+    setText1(name1)
+  }
+  const setVal=()=>{
+    onpress(student)
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  }
   return (
-
-    <View style={styles.my}>
-      <Text>Hello world</Text>
+    <View>
+      <Text style={{fontSize:count}} >{text1}</Text>
+      <Button title='press' onPress={onpress1}></Button>
+      <Button title='press' onPress={setVal}></Button>
+      
     </View>
-    
-  );
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  my:{
-    backgroundColor:'#12f212',
-    borderRadius:22,
-    // alignContent:'center',
-    justifyContent:'center',
-    padding:10,
-    margin:10,
-    borderStyle:'solid',
-    borderBlockColor:'Black',
-    borderWidth:1,
-    alignItems:'center',
 
-    height:100,
-    width:100
-
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+})
---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+const data1="hello from login"
+const login = () => {
+  return (
+    <View>
+      <Text>login{data1}</Text>
+    </View>
+  )
+}
+
+export default login
+
+const styles = StyleSheet.create({})
